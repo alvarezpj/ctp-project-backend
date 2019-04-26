cc06b9166f125e20748d9fe987815c27d633db41
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2019-04-26 02:46:10 -0400

    Add database variables

--- src/config/nodenv.js
+++ src/config/nodenv.js
@@ -1,10 +1,14 @@
 const dotenv = require('dotenv');
-
-
 dotenv.config();
 
 // export environment variables
 module.exports = {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
+    node_env:   process.env.NODE_ENV,
+    port:       process.env.PORT,
+    // database variables
+    database:   process.env.DB_NAME,
+    username:   process.env.DB_USER,
+    password:   process.env.DB_PASS,
+    host:       process.env.DB_HOST,
+    dialect:    process.env.DB_TYPE,
 };
