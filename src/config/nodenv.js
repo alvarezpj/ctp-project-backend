932b2906e954f09201bd4d99eb2fc098e7c3571e
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2019-04-27 14:46:38 -0400

    Add secret and meetup_api_key variables

--- src/config/nodenv.js
+++ src/config/nodenv.js
@@ -3,10 +3,11 @@ dotenv.config();
 
 // export environment variables
 module.exports = {
-    node_env:   process.env.NODE_ENV,
-    port:       process.env.PORT,
-    // database variables
-    database:   process.env.DB_NAME,
-    username:   process.env.DB_USER,
-    password:   process.env.DB_PASS,
+    node_env        :   process.env.NODE_ENV,
+    port            :   process.env.PORT,
+    database        :   process.env.DB_NAME,
+    username        :   process.env.DB_USER,
+    password        :   process.env.DB_PASS,
+    secret          :   process.env.JWT_SECRET,
+    meetup_api_key  :   process.env.MEETUP_API_KEY,
 };
